296acd9763dc9e2ea33e33a9a649d386b4d64a16
Pablo Millaquipay <pabloignacio.millaquipay@alumnos.ulagos.cl>
2026-09-08 13:42:47 -0300

    proyecto web

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import CustomButton from "./components/atoms/Button/CustomButton";
+
+function App() {
+  return (
+    <CustomButton />
+  );
+}
+
+export default App;
